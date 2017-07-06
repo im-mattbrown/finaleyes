9493943383526c1b958f36abc2e986fa4953c706
mattericbrown <matt.eric.brown@gmail.com>
2017-07-06 16:41:53 -0700

    db relations set for the sample rfp. Gems added, testing files created

--- app/models/rfp.rb
+++ app/models/rfp.rb
@@ -1,3 +1,13 @@
 class Rfp < ApplicationRecord
+  has_one :additional_document
+  has_one :application_record
+  has_one :contract_term
+  has_one :dates
+  has_one :definition
   has_one :organization_info
+  has_one :proposer_and_seller_info
+  has_one :purchasing_processes
+  has_one :rfp_detail
+  has_one :selection
+  has_one :t_o_c
 end
